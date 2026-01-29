5bbd2e5f4e3fc1720326c757cdebaa0acc132741
Miguel Diano <francismigueldiano@gmail.com>
2026-01-29 14:25:55 +0800

    The start of my React journey

---
+++ react-app/src/App.tsx
@@ -0,0 +1,16 @@
+import Message from './Message';
+  
+function App()
+{
+  const name = "Miggy";
+  
+  
+
+  if(name)
+  return <div style={{fontFamily: "Times New Roman"}}> <h1>Hello World</h1> </div>
+  
+  return <h1>Hello World</h1>
+
+}
+
+export default App; 
---
+++ react-app/src/Message.tsx
@@ -0,0 +1,9 @@
+//Generally use PascalCasing for Naming Convention
+function Message()
+{
+    // JSX: JavaScript XML
+    return <div style={{backgroundColor: "blue"}}><h1>Hello World</h1></div>;
+    
+}
+
+export default Message;
