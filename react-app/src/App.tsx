3b9c30547294451df32b3608b2507ccd56bc0102
Miguel Diano <francismigueldiano@gmail.com>
2026-01-30 23:34:53 +0800

    Props, States(Highlight on click), .map traverses through each item

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,16 +1,13 @@
-import Message from './Message';
-  
-function App()
-{
-  const name = "Miggy";
-  
-  
+import ListGroup from "./components/ListGroup";
+function App() {
+  let items = ["Miguel", "Imoogi", "Mogi", "Mogi23"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
-  if(name)
-  return <div style={{fontFamily: "Times New Roman"}}> <h1>Hello World</h1> </div>
-  
-  return <h1>Hello World</h1>
-
+  return (
+    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+  );
 }
 
-export default App; 
+export default App;
