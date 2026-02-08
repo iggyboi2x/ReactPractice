413db265d10cccc654f75a972eb88fcaa8169523
Miguel Diano <francismigueldiano@gmail.com>
2026-02-08 18:28:12 +0800

    Click Events

--- brocode-practice-app/src/App.jsx
+++ brocode-practice-app/src/App.jsx
@@ -1,30 +1,10 @@
-import List from "./render-lists/List.jsx";
+import ProfilePicture from "./click-events/ProfilePicture.jsx";
+import Button from "./click-events/Button.jsx";
 
 function App() {
-  const fruits = [
-    { id: 1, name: "Apple", foodRating: 3 },
-    { id: 2, name: "Banana", foodRating: 4 },
-    { id: 3, name: "Coconut", foodRating: 5 },
-    { id: 4, name: "Durian", foodRating: 2 },
-    { id: 5, name: "Elephant", foodRating: 1 },
-  ];
-
-  const vegetables = [
-    { id: 6, name: "Asparagus", foodRating: 3 },
-    { id: 7, name: "Beetroot", foodRating: 4 },
-    { id: 8, name: "Carrot", foodRating: 5 },
-    { id: 9, name: "Dinosaur", foodRating: 2 },
-    { id: 10, name: "Eggplant", foodRating: 1 },
-  ];
-
   return (
     <>
-      <div className="listbody">
-        {fruits.length > 0 ? <List items={fruits} category="Fruits" /> : null}
-        {vegetables.length > 0 ? (
-          <List items={vegetables} category="Vegetables" />
-        ) : null}
-      </div>
+      <ProfilePicture></ProfilePicture>
     </>
   );
 }
---
+++ brocode-practice-app/src/click-events/ProfilePicture.jsx
@@ -0,0 +1,7 @@
+function ProfilePicture() {
+  const imageUrl = "../src/assets/download.webp";
+  const handleClick = (e) => (e.target.style.display = "none");
+  return <img onClick={(e) => handleClick(e)} src={imageUrl}></img>;
+}
+
+export default ProfilePicture;
