3532ad8b3e755f30f893cf1e21479da7e58df488
Miguel Diano <francismigueldiano@gmail.com>
2026-03-04 21:46:19 +0800

    Day 15 Done: Controlled Inputs

--- day15-loginform/src/App.jsx
+++ day15-loginform/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!userName || !password) {
+      alert("Please input valid fields");
+      return;
+    }
+
+    alert(`Welcome, ${userName}`);
+    setUserName("");
+    setPassword("");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="User Name..."
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
